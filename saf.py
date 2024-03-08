5382c5df679665d8cbd8172f07da57e243846464
Mikko Partio <mikko.partio@fmi.fi>
2024-03-08 14:47:22 +0200

    Read or compute mean and std
    Not the best implemenation, might run out of memory

--- saf.py
+++ saf.py
@@ -7,7 +7,7 @@ from enum import Enum
 import copy
 import torch
 from configs import get_args
-
+import os
 from torch.utils.data import IterableDataset
 
 args = get_args()
@@ -15,20 +15,45 @@ args = get_args()
 
 def read_xarray_dataset(dirname):
     ds = xr.open_mfdataset(
-        "{}/*.zarr".format(dirname), engine="zarr", data_vars=["effective_cloudiness"]
+        "{}/*.zarr".format(dirname), engine="zarr", data_vars=args.parameters
     )
-    print(ds)
     return ds
 
 
-def create_generators(train_val_split=0.8, n_hist=4, n_pred=1):
-    ds = read_xarray_dataset(args.dataseries_directory)
+def get_mean_std(ds):
+    data = ds.to_array().values
+
+    if len(data.shape) == 3:
+        data = np.expand_dims(data, axis=1)
+
+    mean = np.mean(data, axis=(1, 2, 3))
+    std = np.std(data, axis=(1, 2, 3))
+
+    return mean, std
+
+
+def create_generators(train_val_split=0.8):
+    if args.dataseries_directory is not None:
+        print("Reading data from directory: {}".format(args.dataseries_directory))
+        ds = read_xarray_dataset(args.dataseries_directory)
+    elif args.dataseries_file is not None:
+        print("Reading data from file: {}".format(args.dataseries_file))
+        ds = xr.open_dataset(args.dataseries_file)
+    else:
+        raise ValueError("No dataseries file or directory specified")
+
+    if os.path.exists("parameter_mean.pt"):
+        mean = torch.load("parameter_mean.pt")
+        std = torch.load("parameter_std.pt")
+    else:
+        mean, std = get_mean_std(ds[args.parameters])
+        torch.save(mean, "parameter_mean.pt")
+        torch.save(std, "parameter_std.pt")
 
     ds_len = len(ds["time"])
-    sample_length = n_hist + n_pred
+    sample_length = args.n_hist + args.n_pred
 
     indexes = np.arange(0, ds_len - sample_length, sample_length)
-    #    np.random.shuffle(indexes)
 
     train_len = int(len(indexes) * train_val_split)
     train_indexes = indexes[0:train_len]
@@ -47,8 +72,12 @@ def create_generators(train_val_split=0.8, n_hist=4, n_pred=1):
     train_ds = ds.isel(time=train_indexes)
     val_ds = ds.isel(time=val_indexes)
 
-    train_gen = SAFDataGenerator(ds=train_ds, n_hist=n_hist, n_pred=n_pred)
-    val_gen = SAFDataGenerator(ds=val_ds, n_hist=n_hist, n_pred=n_pred)
+    train_gen = SAFDataGenerator(
+        ds=train_ds,
+        mean=mean,
+        std=std,
+    )
+    val_gen = SAFDataGenerator(ds=val_ds, mean=mean, std=std)
 
     print("Train generator number of samples: {}".format(len(train_gen)))
     print("Validation generator number of samples: {}".format(len(val_gen)))
@@ -61,34 +90,66 @@ class SAFDataGenerator:
         for k, v in kwargs.items():
             setattr(self, k, v)
 
-        self.sample_length = self.n_hist + self.n_pred
+        self.sample_length = args.n_hist + args.n_pred
+
+    def __str__(self):
+        return "SAFDataGenerator: {}".format(self.__dict__)
 
     def __len__(self):
-        # """Return number of batches in this dataset"""
-        # return len(self.placeholder) // self.batch_size
         """Return number of samples in this dataset"""
         return int(len(self.ds.time) / self.sample_length)
 
     def __getitem__(self, idx):
         indexes = np.arange(idx, idx + self.sample_length)
-        data = self.ds["effective_cloudiness"][indexes, :, :].values
+        data = self.ds.isel(time=indexes)[args.parameters].to_array().values
 
-        x = torch.from_numpy(data[:self.n_hist]).contiguous()
-        y = torch.from_numpy(data[self.n_hist:]).contiguous()
+        if len(data.shape) == 3:
+            data = np.expand_dims(data, axis=1)
+        else:
+            data = np.moveaxis(data, 0, 1)
+
+
+        x = torch.from_numpy(data[: args.n_hist]).contiguous()
+        y = torch.from_numpy(data[args.n_hist :]).contiguous()
+
+        y = torch.squeeze(y, 0)
+
+        if len(x.shape) > 3:
+            x = x.reshape(-1, args.input_size[0], args.input_size[1])
+
+        assert len(x.shape) == 3, f"x shape is not (x,x,x), its {x.shape}"
+        assert len(y.shape) == 3, f"y shape is not (x,x,x), its {y.shape}"
 
         if y.shape == args.input_size:
             y = torch.unsqueeze(y, 0)
 
         assert x.shape == (
-            self.n_hist,
+            args.n_hist * len(args.parameters),
             args.input_size[0],
             args.input_size[1],
-        ), f"x shape is {x.shape}, should be ({self.n_hist}, {args.input_size[0]}, {args.input_size[1]})"
+        ), f"x shape is {x.shape}, should be ({args.n_hist * len(args.parameters)}, {args.input_size[0]}, {args.input_size[1]})"
         assert y.shape == (
-            self.n_pred,
+            args.n_pred * len(args.parameters),
             args.input_size[0],
             args.input_size[1],
-        ), f"y shape is {y.shape}, should be ({self.n_pred}, {args.input_size[0]}, {args.input_size[1]})"
+        ), f"y shape is {y.shape}, should be ({args.n_pred * len(args.parameters)}, {args.input_size[0]}, {args.input_size[1]})"
+
+        # torch.Size([1, 2, 224, 224])
+        # to
+        # torch.Size([1, 50176, 2])
+
+        x_orig_shape = x.shape
+        y_orig_shape = y.shape
+        x = x.reshape(args.n_hist, len(args.parameters), -1).permute(0, 2, 1)
+        y = y.reshape(args.n_pred, len(args.parameters), -1).permute(0, 2, 1)
+
+        x = (x - self.mean) / self.std
+        y = (y - self.mean) / self.std
+
+        # .. and back to original shape
+
+        x = x.permute(0, 2, 1).reshape(x_orig_shape)
+        y = y.permute(0, 2, 1).reshape(y_orig_shape)
 
         return (x, y)
 
@@ -107,142 +168,3 @@ class SAFDataset(IterableDataset):
 
     def __len__(self):
         return len(self.generator)
-
-
-# class SAFDataLoader:
-#    def __init__(self, **kwargs):
-#        self.img_size = (args.input_size, args.input_size)
-#        self.include_datetime = False
-#        self.include_topography = False
-#        self.include_terrain_type = False
-#        self.include_sun_elevation_angle = False
-#        self.dataseries_file = args.dataseries_file
-#        self.batch_size = args.batch_size
-#
-#        self._placeholder = []
-#        self.leadtimes = []
-#
-#        self.initialize()
-#
-#    def initialize(self):
-#        # Read static datas, so that each dataset generator
-#        # does not have to read them
-#
-#        if args.leadtime_conditioning:
-#            self.leadtimes = np.asarray(
-#                [
-#                    np.full(self.img_size, x / args.leadtime_conditioning).astype(
-#                        np.float32
-#                    )
-#                    for x in range(args.leadtime_conditioning)
-#                ]
-#            )
-#
-#        if self.include_topography:
-#            self.topography_data = np.expand_dims(
-#                create_topography_data(self.img_size), axis=0
-#            )
-#
-#        if self.include_terrain_type:
-#            self.terrain_type_data = np.expand_dims(
-#                create_terrain_type_data(self.img_size), axis=0
-#            )
-#
-#        if self.include_sun_elevation_angle:
-#            if self.operating_mode == OpMode.INFER:
-#                self.sun_elevation_angle_data = {}
-#                for i in range(args.n_pred):
-#                    ts = self.analysis_time + timedelta(minutes=(1 + i) * 15)
-#                    self.sun_elevation_angle_data[
-#                        ts.strftime("%Y%m%dT%H%M%S")
-#                    ] = create_sun_elevation_angle(ts, (128, 128))
-#            else:
-#                self.sun_elevation_angle_data = create_sun_elevation_angle_data(
-#                    self.img_size,
-#                )
-#
-#        # create placeholder data
-#
-#        self.xds = read_xarray_dataset(args.dataseries_directory)
-#
-#        i = 0
-#
-#        step = args.n_hist + args.n_pred
-#        n_fut = args.n_pred
-#
-#        assert (
-#            len(self.elements) - step
-#        ) >= 0, "Too few data to make a prediction: {} (need at least {})".format(
-#            len(self.elements), step
-#        )
-#
-#        if args.leadtime_conditioning == 0:
-#            while i <= len(self.elements) - step:
-#                x = list(self.elements[i : i + args.n_hist])
-#                y = list(self.elements[i + args.n_hist : i + step])
-#
-#                self._placeholder.append([x, y])
-#
-#                i += step
-#
-#        else:
-#            step = args.n_hist + args.leadtime_conditioning
-#            while i <= len(self.elements) - step:
-#                x = list(self.elements[i : i + args.n_hist])
-#
-#                for lt in range(args.leadtime_conditioning):
-#                    x_ = copy.deepcopy(x)
-#                    x_.append(lt)
-#                    # x_.append(self.include_datetime)
-#                    # x_.append(self.include_topography)
-#                    # x_.append(self.include_terrain_type)
-#                    # x_.append(self.include_sun_elevation_angle)
-#                    y = [self.elements[i + args.n_hist + lt]]
-#
-#                    self._placeholder.append([x_, y])
-#
-#                i += step
-#
-#        assert len(self._placeholder) > 0, "Placeholder array is empty"
-#
-#        print(
-#            "Placeholder timeseries length: {} number of samples: {} sample length: x={},y={} example sample: {}".format(
-#                len(self.elements),
-#                len(self._placeholder),
-#                len(self._placeholder[0][0]),
-#                len(self._placeholder[0][1]),
-#                self._placeholder[np.random.randint(len(self._placeholder))],
-#            )
-#        )
-#
-#        np.random.shuffle(self._placeholder)
-#
-#    def __len__(self):
-#        """Return number of samples"""
-#        return len(self._placeholder)
-#
-#    def get_dataset(self, take_ratio=None, skip_ratio=None):
-#        placeholder = None
-#
-#        if take_ratio is not None:
-#            l = int(len(self._placeholder) * take_ratio)
-#            placeholder = self._placeholder[0:l]
-#
-#        if skip_ratio is not None:
-#            l = int(len(self._placeholder) * skip_ratio)
-#            placeholder = self._placeholder[l:]
-#
-#        if placeholder is None:
-#            placeholder = copy.deepcopy(self._placeholder)
-#
-#        assert len(placeholder) > 0
-#
-#        gen = SAFDataGenerator(
-#            # placeholder=placeholder,
-#            leadtimes=self.leadtimes,
-#            batch_size=args.batch_size,
-#        )
-#
-#        dataset = SAFDataset(gen)
-#
-#        return dataset
