5b116e323a481d4ad8138e2105c721c92797445a
Mikko Partio <mikko.partio@fmi.fi>
2024-03-24 13:57:38 +0200

    Changes

--- pangu_test.py
+++ pangu_test.py
@@ -6,7 +6,7 @@ from pangu_model import Pangu, Pangu_lite
 from tqdm import tqdm
 from configs import get_args
 from streaming_tensor import StreamingTensor
-from pangu_utils import split_surface_data, split_upper_air_data
+from pangu_utils import split_surface_data, split_upper_air_data, split_weights
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime
@@ -26,26 +26,26 @@ def model_forward(m, input_surface, surface_mask, input_upper_air, mean, std):
         output_surface, output_upper_air = m(
             input_surface, surface_mask, input_upper_air
         )
-        output_surface = output_surface.detach().cpu() # B C H W
-        output_upper_air = output_upper_air.detach().cpu().squeeze(2) # B C Z H W
+        output_surface = output_surface.detach().cpu()  # B C H W
+        output_upper_air = output_upper_air.detach().cpu().squeeze(2)  # B C Z H W
         # y = torch.cat((output_surface, output_upper_air), dim=3)
 
         plt.imshow(np.squeeze(output_surface[:, 0, :, :]), cmap="gray_r")
         plt.savefig("images/outputs_{:02d}.png".format(i))
 
-        #if len(y.shape) == 3:
+        # if len(y.shape) == 3:
         #    y = y.unsqueeze(1)
 
         # de-normalize
-        output_surface = torch.permute(output_surface, (0, 2, 3, 1)) # B H W C
+        output_surface = torch.permute(output_surface, (0, 2, 3, 1))  # B H W C
         output_surface = output_surface * std + mean
-        output_surface = torch.permute(output_surface, (0, 3, 1, 2)) # B C H W
+        output_surface = torch.permute(output_surface, (0, 3, 1, 2))  # B C H W
         outputs.append(output_surface.numpy())
 
         # x = x[:, 1:, ...]
         # x = torch.cat((x, y), dim=1)
-    outputs = np.stack(outputs).squeeze(0)
-    print(outputs.shape)
+
+    outputs = np.stack(outputs)  # .squeeze(0)
     assert len(outputs.shape) == 4
 
     return outputs
@@ -65,17 +65,21 @@ def test(model, test_gen):
     """Training code"""
     # Prepare for the optimizer and scheduler
 
-    parameter_mean = torch.load("parameter_mean.pt")
-    parameter_std = torch.load("parameter_std.pt")
+    #    parameter_mean = torch.load("parameter_mean.pt")
+    #    parameter_std = torch.load("parameter_std.pt")
 
-    surface_mean = parameter_mean[0].to(args.device)
-    upper_mean = parameter_mean[1:].to(args.device)
-    surface_std = parameter_std[0].to(args.device)
-    upper_std = parameter_std[1:].to(args.device)
+    surface_mean, upper_air_mean = split_weights(test_gen.mean)
+    surface_std, upper_air_std = split_weights(test_gen.std)
 
-    surface_mask = torch.ones(
-        args.batch_size, 3, args.input_size[0], args.input_size[1]
-    ).to(args.device)
+    # surface_mean = parameter_mean[0].to(args.device)
+    # upper_mean = parameter_mean[1:].to(args.device)
+    # surface_std = parameter_std[0].to(args.device)
+    # upper_std = parameter_std[1:].to(args.device)
+
+    lsm = test_gen.get_static_features("lsm_heightAboveGround_0")
+    z = test_gen.get_static_features("z_heightAboveGround_0")
+    surface_mask = torch.stack([lsm, z]).to(args.device)
+    surface_mask = surface_mask.unsqueeze(0).repeat(args.batch_size, 1, 1, 1)
 
     model.eval()
 
@@ -91,8 +95,8 @@ def test(model, test_gen):
                 input_surface,
                 surface_mask,
                 input_upper_air,
-                test_gen.mean,
-                test_gen.std,
+                surface_mean,
+                surface_std,
             )
 
             # output = np.concatenate((analysis, output), axis=1)
@@ -109,8 +113,12 @@ def test(model, test_gen):
 
 
 if __name__ == "__main__":
-    # m = Pangu_lite().to(args.device)
-    m = Pangu().to(args.device)
+    if args.model_name == "pangu_lite":
+        m = Pangu_lite().to(args.device)
+    elif args.model_name == "pangu":
+        m = Pangu().to(args.device)
+    else:
+        raise ValueError("Unknown model name: {}".format(args.model_name))
 
     m.load_state_dict(
         torch.load(
@@ -119,5 +127,5 @@ if __name__ == "__main__":
         )
     )
 
-    test_gen = StreamingTensor(args.dataseries_file, args.parameters)
+    test_gen = StreamingTensor(args.dataseries_file, args.parameters, args.model_dir)
     predictions, times = test(m, test_gen)
