4cf1073809adb2938888bf306d2f205c37c78309
Mikko Partio <mikko.partio@fmi.fi>
2024-05-08 13:07:59 +0300

    Update default parameters

--- configs.py
+++ configs.py
@@ -1,11 +1,59 @@
 import argparse
 
+
 def parse_size(s):
     try:
         w, h = map(int, s.split("x"))
         return w, h
     except:
         raise ValueError("Size must be W,H")
+
+
+def default_params():
+    params = [
+        "effective-cloudiness_heightAboveGround_0",
+        "mld_heightAboveGround_0",
+        "pres_heightAboveGround_0",
+        "fgcorr_heightAboveGround_10",
+        "rcorr_heightAboveGround_2",
+        "t_heightAboveGround_0",
+        "tcorr_heightAboveGround_2",
+        "ucorr_heightAboveGround_10",
+        "vcorr_heightAboveGround_10",
+        "pres_heightAboveSea_0",
+        "r_isobaricInhPa_300",
+        "t_isobaricInhPa_300",
+        "u_isobaricInhPa_300",
+        "v_isobaricInhPa_300",
+        "z_isobaricInhPa_300",
+        "r_isobaricInhPa_500",
+        "t_isobaricInhPa_500",
+        "u_isobaricInhPa_500",
+        "v_isobaricInhPa_500",
+        "z_isobaricInhPa_500",
+        "r_isobaricInhPa_700",
+        "t_isobaricInhPa_700",
+        "u_isobaricInhPa_700",
+        "v_isobaricInhPa_700",
+        "z_isobaricInhPa_700",
+        "r_isobaricInhPa_850",
+        "t_isobaricInhPa_850",
+        "u_isobaricInhPa_850",
+        "v_isobaricInhPa_850",
+        "z_isobaricInhPa_850",
+        "r_isobaricInhPa_925",
+        "t_isobaricInhPa_925",
+        "u_isobaricInhPa_925",
+        "v_isobaricInhPa_925",
+        "z_isobaricInhPa_925",
+        "r_isobaricInhPa_1000",
+        "t_isobaricInhPa_1000",
+        "u_isobaricInhPa_1000",
+        "v_isobaricInhPa_1000",
+        "z_isobaricInhPa_1000",
+    ]
+    return params
+
 
 def get_args():
     parser = argparse.ArgumentParser("Swin training and evaluation", add_help=False)
@@ -43,7 +91,7 @@ def get_args():
     parser.add_argument(
         "--parameters",
         nargs="+",
-        default="effective_cloudiness_heightAboveGround_0",
+        default=default_params(),
         help="Parameters to use for training",
     )
     parser.add_argument(
